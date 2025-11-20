cb43e57eff7374aa89ec5756e7a0932d1fc7e499
Kennedy MBA <shaltonkennedy@gmail.com>
2025-11-20 13:53:52 +0100

    Update init.py

--- app/init.py
+++ app/init.py
@@ -1,1 +1,9 @@
-#vide mais nécessaire
+from app.nlp_module import analyse_document
+
+
+def test_analyse_document():
+    texte = "J'ai reçu une amende qui contient une erreur administrative sur la date."
+    label = analyse_document(texte)
+    assert label in [
+        'erreur administrative', 'violation du droit', 'information incomplète', 'aucun problème détecté
+    ]
